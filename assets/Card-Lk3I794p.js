36b1718722785816639e0f82e3292990bbc18a07
Maksim <Daymansolvatore88@gmail.com>
2025-12-09 22:05:37 +0400

    Updates

---
+++ assets/Card-Lk3I794p.js
@@ -0,0 +1,1 @@
+import{j as t,l as m}from"./mui-vendor-CLtSI9R1.js";import"./index-p_fKfQYs.js";const i=({className:r="",children:s,...a})=>t.jsx(m,{className:`card ${r}`.trim(),...a,children:s});export{i as C};
