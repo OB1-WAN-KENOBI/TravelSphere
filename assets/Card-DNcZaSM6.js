073b557fe4847a972fe0367cbf327d47fd1b5c42
Maksim <Daymansolvatore88@gmail.com>
2025-12-09 22:30:47 +0400

    Updates

---
+++ assets/Card-DNcZaSM6.js
@@ -0,0 +1,1 @@
+import{j as t,l as m}from"./mui-vendor-CJVoYIqO.js";import"./index-CCwr1TxZ.js";const i=({className:r="",children:s,...a})=>t.jsx(m,{className:`card ${r}`.trim(),...a,children:s});export{i as C};
